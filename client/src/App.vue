963b370aa2e6a0d25095b9866d667353b2fb95c5
natnaelawel <natnael.awel@gmail.com>
2020-08-02 11:36:26 +0300

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <app-nav></app-nav>
+    </div>
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppNav from "@/components/AppNav";
+
+export default {
+  components: {
+    "app-nav": AppNav
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community!</h1>
+      <p class="lead">A fully open source forum made with ♥</p>
+      <hr class="my-4" />
+      <p>This forum is all about the community. Be Kind Be helpfull. 🌈 </p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  components: {
+  }
+};
+</script>
